dc1c68873c94d29581653dd808b0219455af96d4
Subham Shyamal Bharadwaz <72348711+subhamBharadwaz@users.noreply.github.com>
2023-07-20 13:05:24 +0530

    chore: :mag: Improve SEO

---
+++ src/components/copy-button.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import * as React from "react"
+import { CheckIcon, CopyIcon } from "@radix-ui/react-icons"
+
+import { Event, trackEvent } from "@/lib/events"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+
+interface CopyButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  value: string
+  src?: string
+  event?: Event["name"]
+}
+
+async function copyToClipboardWithMeta(value: string, event?: Event) {
+  navigator.clipboard.writeText(value)
+  if (event) {
+    trackEvent(event)
+  }
+}
+
+export function CopyButton({
+  value,
+  className,
+  src,
+  event,
+  ...props
+}: CopyButtonProps) {
+  const [hasCopied, setHasCopied] = React.useState(false)
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setHasCopied(false)
+    }, 2000)
+  }, [hasCopied])
+
+  return (
+    <Button
+      size="icon"
+      variant="ghost"
+      className={cn(
+        "relative z-10 h-6 w-6 text-zinc-50 hover:bg-zinc-700 hover:text-zinc-50",
+        className
+      )}
+      onClick={() => {
+        copyToClipboardWithMeta(
+          value,
+          event
+            ? {
+                name: event,
+                properties: {
+                  code: value,
+                },
+              }
+            : undefined
+        )
+        setHasCopied(true)
+      }}
+      {...props}
+    >
+      <span className="sr-only">Copy</span>
+      {hasCopied ? (
+        <CheckIcon className="h-3 w-3" />
+      ) : (
+        <CopyIcon className="h-3 w-3" />
+      )}
+    </Button>
+  )
+}
