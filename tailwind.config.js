4f35bea1bfff95b3d7f4fea9efb206db9662f983
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2024-08-03 20:32:57 +0530

    chore: :construction: Regular Modifications
    - Added docker-compose for local development

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
 const { fontFamily } = require("tailwindcss/defaultTheme")
 
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette")
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"))
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  )
+
+  addBase({
+    ":root": newVars,
+  })
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -157,5 +173,10 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar-hide"),
+    addVariablesForColors,
+  ],
 }
